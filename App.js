4f0dbac814ff88eee16ef7e9f45b7b75bd38efd3
SHAIKASIFALI <12006051@svuceedu.onmicrosoft.com>
2023-10-09 00:49:34 +0530

    mocked the html structure with react apis and added the App.js files

---
+++ App.js
@@ -0,0 +1,29 @@
+// const heading = React.createElement('h1',{},'hello world from react');
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(heading);
+
+// Mock this using ReactApis
+
+/* <div>
+      <h1>Hello World</h1>
+      <div>
+        <h2>Subheading 1</h2>
+        <div>
+          <h3>Subheading 2</h3>
+        </div>
+      </div>
+    </div> */
+
+ const parent = React.createElement("div",{},[
+    React.createElement('h1',{},"Hello World!"),
+    React.createElement("div",{},[
+        React.createElement('h2',{},'subheading1'),
+        React.createElement('div',{},React.createElement('h3',{},'subheading3'))
+    ])
+ ])
+
+ console.log(parent); // here the parent is the javascript object
+
+ const root = ReactDOM.createRoot(document.getElementById('root'));
+
+ root.render(parent); // now the js object converts to the html code.. understood by the browser.
